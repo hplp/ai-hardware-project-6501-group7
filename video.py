da31f86578652a6fef3d77e156fd3e51efb1a910
Ezeuko Emmanuel <ezeuko.arinze@gmail.com>
2024-12-04 19:50:40 +0100

    Update video.py

--- video.py
+++ video.py
@@ -1,5 +1,5 @@
 import cv2
-from test import *
+from predict import *
 # Open the default camera
 cam = cv2.VideoCapture(0)
 
@@ -14,14 +14,23 @@ out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 while True:
     ret, frame = cam.read()
 
+    if not ret:
+        print("Failed to capture frame. Exiting...")
+        break
+
     # Write the frame to the output file
     out.write(frame)
 
     # Display the captured frame
     cv2.imshow('Camera', frame)
+
+    # Save the frame as image.png when 't' is pressed
     if cv2.waitKey(1) == ord('t'):
+        cv2.imwrite('img.png', frame)
+        print("Captured frame saved as image.png")
         start_test()
-    # Press 'q' to exit the loop
+
+    # Press 's' to exit the loop
     if cv2.waitKey(1) == ord('s'):
         break
 
